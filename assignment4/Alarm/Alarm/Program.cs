bb1c0dc5138d2afe2f95df2c58e1d46b3eaa6703
unknown <Z13181821572@163.com>
2025-03-13 22:10:52 +0800

    assignment4

---
+++ assignment4/Alarm/Alarm/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Alarm
+{
+    //定义滴答事件
+    public event EventHandler Tick;
+    //定义响铃事件
+    public event EventHandler AlarmRing;
+
+    public void Start()
+    {
+        //模拟闹钟走时
+        for(int i=0;i<10;i++)
+        {
+            OnTick(EventArgs.Empty);
+            System.Threading.Thread.Sleep(1000);
+        }
+        //触发响铃时间
+        OnAlarmRing(EventArgs.Empty);
+    }
+    protected virtual void OnTick(EventArgs e)
+    {
+        //触发滴答事件，并显示在控制台
+        Tick.Invoke(this, e);
+        Console.WriteLine("闹钟在走时");
+    }
+    protected virtual void OnAlarmRing(EventArgs e)
+    {
+        //触发响铃事件，并显示在控制台
+        AlarmRing.Invoke(this, e);
+        Console.WriteLine("闹钟响铃啦，该起床了");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Alarm alarm = new Alarm();
+        //订阅滴答事件
+        alarm.Tick += (sender, args) =>
+        {
+            Console.WriteLine();
+        };
+        //订阅响铃事件
+        alarm.AlarmRing += (sender, args) =>
+        {
+            Console.WriteLine();
+        };
+        alarm.Start();
+        Console.ReadLine();
+    }
+}
---
+++ assignment4/LinkList/LinkList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+} 
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public void Add(T t)
+    {
+        Node<T> n=new Node<T>(t);
+        if(tail==null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while(current!=null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class LinkList
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> intlist = new GenericList<int>();
+        for(int x=0;x<10;x++)
+        {
+            intlist.Add(x);
+        }
+        //打印链表元素
+        intlist.ForEach(x => Console.Write(x + " "));
+        Console.WriteLine();
+
+        //最大值
+        int max = int.MinValue;
+        intlist.ForEach(x => max = Math.Max(max, x));
+        Console.WriteLine($"最大值为{max}");
+
+        //最小值
+        int min=int.MaxValue;
+        intlist.ForEach(x => min = Math.Min(min, x));
+        Console.WriteLine($"最小值为{min}");
+
+        //求和
+        int sum = 0;
+        intlist.ForEach(x => sum += x);
+        Console.WriteLine($"和为{sum}");
+    }
+}
